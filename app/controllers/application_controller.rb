51b5359948cc4bf3006cad98fa0f238eca1479b5
Garrett <garrettgsb@gmail.com>
2017-01-14 14:44:45 -0800

    Blocked out routes for history for tenant, landlord and property

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,18 @@ class ApplicationController < ActionController::Base
     value = { helloworld: "Hello, world!" }
     render json: value
   end
+
+  def tenant_history
+    # One object for each property that they've stayed at
+  end
+
+  def landlord_history
+    # One object per property that they own
+  end
+
+  def property_history
+    # One aggregate object of all tenancies
+  end
+
+
 end
