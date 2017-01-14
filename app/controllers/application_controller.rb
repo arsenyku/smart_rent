c93202c3afec8f7932f2e9a038ecfd0cc4b3db12
ashleyyy <ashleyfisher@gmail.com>
2017-01-14 15:06:54 -0800

    added property history

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,51 @@ class ApplicationController < ActionController::Base
     value = { helloworld: "Hello, world!" }
     render json: value
   end
+
+  def history
+    render json: {
+      "property1": {
+        "tenant": "Joe Schmoe",
+        "duration_of_lease": 4,
+        "requisitions": {
+          "req1": {
+            "issue": "plumbing",
+            "response_time": 5,
+            "resolved": true
+          },
+          "req2": {
+            "issue": "roaches",
+            "response_time": null,
+            "resolved": false
+          },
+          "req3": {
+            "issue": "nachos",
+            "response_time": 1,
+            "resolved": true
+          }
+        }
+      },
+      "property2": {
+        "tenant": "Lisa Simpson",
+        "duration_of_lease": 12,
+        "requisitions": {
+          "req1": {
+            "issue": "snow removal",
+            "response_time": 1,
+            "resolved": true
+          },
+          "req2": {
+            "issue": "snow removal",
+            "response_time": 2,
+            "resolved": true
+          }
+        }
+      },
+      "property3": {
+        "tenant": "Sarah Foot",
+        "duration_of_lease": 0,
+        "requisitions": {}
+      }
+    }
+  end
 end
---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,7 @@
+class EventController < ApplicationController
+
+  def index 
+    render json: "hi"
+  end
+
+end
