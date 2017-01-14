36dc3211a9306e49d6df83aca7dee44ff54ae463
Garrett <garrettgsb@gmail.com>
2017-01-14 14:35:12 -0800

    Set up hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def helloworld
+    value = { helloworld: "Hello, world!" }
+    render json: value
+  end
 end
